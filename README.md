7cc0a25eb6eef5ff83497451ed31b2cb8e4c1e55
binku <binku87@gmail.com>
2017-10-27 14:17:01 +0800

    add resource/action with error case

---
+++ README.md
@@ -0,0 +1,11 @@
+## How to run the case
+
+### Setup test DB
+
+cd $GOPATH/src/github.com/qor/qor-example-cases
+docker-compose -f docker-compose.yml up -d
+
+### Run test case
+
+cd CASE-PATH
+go run main.go
---
+++ resource/actions/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/qor/admin"
+	"github.com/qor/roles"
+	appkitlog "github.com/theplant/appkit/log"
+	"github.com/theplant/appkit/server"
+)
+
+type Order struct {
+	gorm.Model
+	Num   string
+	Price float64
+}
+
+// run with dummy data
+// MODE=data go run main.go
+
+func main() {
+	db, err := gorm.Open("postgres", "user=qor_test password=123 dbname=qor_test sslmode=disable host=localhost port=6000")
+	if err != nil {
+		panic(err)
+	}
+	if os.Getenv("MODE") == "data" {
+		db.DropTable(&Order{})
+		db.AutoMigrate(&Order{})
+		db.Create(&Order{Num: "T00001", Price: 1000})
+		db.Create(&Order{Num: "T00002", Price: 1500})
+	} else {
+		db.AutoMigrate(&Order{})
+	}
+
+	adm := admin.New(&admin.AdminConfig{DB: db})
+	orderR := adm.AddResource(&Order{})
+	orderR.Action(&admin.Action{
+		Name: "Action with error",
+		Handler: func(argument *admin.ActionArgument) error {
+			argument.Context.AddError(fmt.Errorf("This is a error"))
+			return fmt.Errorf("This is a error")
+		},
+		Modes:      []string{"edit", "index", "show"},
+		Permission: roles.Allow(roles.CRUD, roles.Anyone),
+	})
+
+	mux := http.NewServeMux()
+	adm.MountTo("/admin", mux)
+	color.Green("URL: %v", "http://localhost:3000/admin/orders")
+	server.ListenAndServe(server.Config{Addr: ":3000"}, appkitlog.Default(), mux)
+}
