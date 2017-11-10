d70851ff3d42199003782bf51482031701232dd4
Jinzhu <wosmvp@gmail.com>
2017-11-10 17:44:04 +0800

    Make scopes default example reasonable

--- resource/scopes_default/main.go
+++ resource/scopes_default/main.go
@@ -16,6 +16,7 @@ import (
 type Order struct {
 	gorm.Model
 	Num   string
+	State string
 	Price float64
 }
 
@@ -23,16 +24,25 @@ type Order struct {
 // MODE=data go run main.go
 
 func main() {
-	db, err := gorm.Open("postgres", "user=qor_test password=123 dbname=qor_test sslmode=disable host=localhost port=6000")
+	db, err := gorm.Open("postgres", "user=qor password=qor dbname=qor_test sslmode=disable host=localhost")
 	if err != nil {
 		panic(err)
 	}
 	if os.Getenv("MODE") == "data" {
 		db.DropTable(&Order{})
 		db.AutoMigrate(&Order{})
-		db.Create(&Order{Num: "T00001", Price: 1000})
-		db.Create(&Order{Num: "T00002", Price: 1500})
-		db.Create(&Order{Num: "T00003", Price: 2000})
+		db.Create(&Order{Num: "T00001", State: "pending", Price: 1000})
+		db.Create(&Order{Num: "T00002", State: "confirmed", Price: 1500})
+		db.Create(&Order{Num: "T00003", State: "shipped", Price: 2000})
+		db.Create(&Order{Num: "T00004", State: "cancelled", Price: 2500})
+		db.Create(&Order{Num: "T00005", State: "pending", Price: 2000})
+		db.Create(&Order{Num: "T00006", State: "confirmed", Price: 2000})
+		db.Create(&Order{Num: "T00007", State: "shipped", Price: 2500})
+		db.Create(&Order{Num: "T00008", State: "cancelled", Price: 1500})
+		db.Create(&Order{Num: "T00009", State: "pending", Price: 2500})
+		db.Create(&Order{Num: "T000010", State: "confirmed", Price: 2500})
+		db.Create(&Order{Num: "T000020", State: "shipped", Price: 1000})
+		db.Create(&Order{Num: "T000030", State: "cancelled", Price: 1000})
 	} else {
 		db.AutoMigrate(&Order{})
 	}
@@ -45,37 +55,53 @@ func main() {
 		Default: true,
 		Group:   "State",
 		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
-			return d.Where("price = 1000")
+			return d.Where("state = ?", "pending")
 		},
 	})
 	orderR.Scope(&admin.Scope{
 		Name:  "Confirmed",
 		Group: "State",
 		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
-			return d.Where("price = 1500")
+			return d.Where("state = ?", "confirmed")
 		},
 	})
 
 	orderR.Scope(&admin.Scope{
 		Name:  "Shipped",
-		Group: "Shipping",
+		Group: "State",
 		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
-			return d.Where("price = 1000")
+			return d.Where("state = ?", "shipped")
 		},
 	})
+
 	orderR.Scope(&admin.Scope{
-		Name:    "Box Wrapping",
-		Group:   "Shipping",
+		Name:  "Cancelled",
+		Group: "State",
+		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
+			return d.Where("state = ?", "cancelled")
+		},
+	})
+
+	orderR.Scope(&admin.Scope{
+		Name:    "Cheap",
+		Group:   "Price",
 		Default: true,
 		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
-			return d.Where("price = 1500")
+			return d.Where("price <= 1500")
 		},
 	})
 	orderR.Scope(&admin.Scope{
-		Name:  "Returned",
-		Group: "Shipping",
+		Name:  "Middle",
+		Group: "Price",
 		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
-			return d.Where("price = 2000")
+			return d.Where("price > 1500 AND price <= 2000")
+		},
+	})
+	orderR.Scope(&admin.Scope{
+		Name:  "Expensive",
+		Group: "Price",
+		Handler: func(d *gorm.DB, ctx *qor.Context) *gorm.DB {
+			return d.Where("price > 2000")
 		},
 	})
 
